8494e1d3e5ca888a0d170a5adb133aa57c1bf87d
Rhys Sorenson-Graff <sorensor@whitman.edu>
2024-03-05 23:43:32 -0800

    Add operators, update readme, handled edge cases for super/sub script

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# vue-project
+# Chemical Equation Input
+
+Notes:
+* Options for script levels:
+  * normal
+  * superscript
+  * subscript
+  * both superscript and subscript
+* Highlight a section and clicking superscript or subscript button will change highlighted text to that level.
+* Without highlighting, click superscript or subscript button to move caret to that script level.
+* Without highlighting, clicking both superscript and subscript button will put caret at the normal level. It will create two boxes at each level.
+  * The boxes would ideally be temporary, but I could not figure out a good way of removing them.
+
+Design choices:
+* Highlighting section and clicking button for both superscript and subscript will move all highlighted text to superscript.
+* Operators will always be on the normal level, even if you highlight them and click to change level. However, all other highlighted text will change.
+* Highlighting section with an operator and clicking button for both superscript and subscript will delete the operator.
+
+# Getting Started 
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
   <div class="app">
-    <div contenteditable="true" v-html="inputText" class="text-box" @input="updateText" @click="removePlaceholder"></div>
+    <div contenteditable="true" v-html="inputText" class="text-box" ref="myTextBox" @input="updateText" @click="removePlaceholder" @keydown="updateText" @keyup="updateText" @mouseup="updateText"></div>
     <div class="button-container">
       <button :class="{ 'highlighted': activeButton === 'normal_button' }" @click="surroundTextWithTag('normal_button')">X</button>
       <button :class="{ 'highlighted': activeButton === 'sup_button' }" @click="surroundTextWithTag('sup_button')">X<sup>y</sup></button>
       <button :class="{ 'highlighted': activeButton === 'sub_button' }" @click="surroundTextWithTag('sub_button')">X<sub>y</sub></button>
-      <button :class="{ 'highlighted': activeButton === 'stack_button' }" @click="surroundTextWithboth('stack_button')">X<span class="stacked"><sup>z</sup><sub>y</sub></span></button>
+      <button :class="{ 'highlighted': activeButton === 'stack_button' }" @click="surroundTextWithboth()">X<span class="buttonstacked"><sup>z</sup><sub>y</sub></span></button>
+      <button :class="{ 'highlighted': activeButton === 'arrow_button' }" @click="insertOperator('⇒')">⇒</button>
+      <button :class="{ 'highlighted': activeButton === 'arrow_button' }" @click="insertOperator('⇄')">⇄</button>
+
+      <button :class="{ 'highlighted': activeButton === 'compute_button' }" @click="compute()">Compute</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent } from 'vue';
+
+class NotImplementedError extends Error {
+    constructor(message = 'This feature is not implemented yet.') {
+        super(message);
+        this.name = 'NotImplementedError';
+    }
+}
+
+export default defineComponent({
   data() {
     return {
-      inputText: '<p class="placeholder">Start typing here...</p>',
+      inputText: 'Start typing here...',
       activeButton: 'normal_button',
       displayText: '',
-      tags: new Map([
-        ['sup_button', ['<sup>', '</sup>']],
-        ['sub_button', ['<sub>', '</sub>']],
-        ['normal_button', ['', '']]
-      ])
+      operators: new Set(['⇒', '⇄']),
+      caretPosition: 0,
+      tags: new Map([['sup_button', ['<sup>', '</sup>']],
+                     ['sub_button', ['<sub>', '</sub>']],
+                     ['normal_button', ['', '']]
+                    ])      
     };
   },
   methods: {
@@ -29,40 +43,55 @@ export default {
     surroundTextWithTag(button) {
       const selectedText = this.getSelectedText();
       let newText = '';
-      if (selectedText.length === 0){
+      if (selectedText.length === 0) {
         newText += this.tags.get(button)[0] + '&shy;' + this.tags.get(button)[1]; // WARNING: this uses an invisible character
       } else {
+        newText = this.tags.get(button)[0];
         for (let i = 0; i < selectedText.length; i++) {
-          newText += this.tags.get(button)[0] + selectedText[i] + this.tags.get(button)[1];
+          if (this.operators.has(selectedText[i])){
+            newText += this.tags.get(button)[1] + selectedText[i] + this.tags.get(button)[0];
+          } else {
+            newText += selectedText[i];
+          }
         }
+        newText += this.tags.get(button)[1];
       }
-      
+
       document.execCommand('insertHTML', false, newText);
       this.activeButton = button;
     },
 
-    surroundTextWithboth(button) {
-      var selectedText = this.getSelectedText();
-      if (selectedText.length === 0){
-        // selectedText = '▯ ' + '&shy;';
+    surroundTextWithboth() {
+      var selectedText = this.removeTemplateLiterals(this.getSelectedText());
+      if (selectedText.length === 0) {
         selectedText = '▯ ▯';
       } else if (!selectedText.includes(' ')) {
         selectedText += ' ' + '▯';
       }
-      let newText = '<span class="stacked"><sub>';
+      let newText = '<span class="stacked"><sup>';
       for (let i = 0; i < selectedText.length; i++) {
         if (selectedText[i] === ' ') {
-          newText += '</sub><sup>';
-        } else{
+          newText += '</sup><sub>';
+        } else if (!this.operators.has(selectedText[i])){
           newText += selectedText[i];
         }
       }
-      // newText += '</sup></span>';
-      newText += '</sup></span> ' + '&shy;'; // WARNING: this uses an invisible character 
+      newText += '</sub></span> ' + '&shy;'; // WARNING: this uses an invisible character 
       document.execCommand('insertHTML', false, newText);
       this.activeButton = 'normal_button';
     },
 
+    insertOperator(char_entity) {
+      const selectedText = this.getSelectedText();
+      let newText = '';
+      newText += this.removeTemplateLiterals(char_entity);
+      document.execCommand('insertHTML', false, newText);
+      this.activeButton = 'normal_button';
+    },
+
+    compute(){
+      throw new NotImplementedError();
+    },
 
     getSelectedText() {
       const selection = window.getSelection();
@@ -72,10 +101,43 @@ export default {
       }
       return null;
     },
+    
+    updateCaretPosition() {
+      const selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        const preCaretRange = range.cloneRange();
+        preCaretRange.selectNodeContents(this.$refs.myTextBox);
+        preCaretRange.setEnd(range.endContainer, range.endOffset);
+        this.caretPosition = preCaretRange.toString().length;
+      }
+    },
+
+    removeAdjacent(char){
+      throw new NotImplementedError();
+    },
+
+    normalizeOperators(){
+      for (let i; i < this.displayText.length; i++){
+        console.log("test")
+        if (this.operators.has(this.displayText[i])){
+          this.removeTemplateLiterals(this.displayText[i]);
+        }
+      }
+    },
+
+    removeTemplateLiterals(str) {
+      const regex = /\${(.*?)}/g;
+      return str.replace(regex, '');
+    },
 
     updateText(event) {
       this.displayText = event.target.innerHTML;
-      console.log(`${this.displayText}`); // logs with with HTML template literals (<sup>, </sup>, <sup>, and </sup>)
+      this.normalizeOperators();
+      // this.updateCaretPosition();
+      // this.removeAdjacent('▯') 
+
+      console.log(`${this.displayText}`); // logs with with HTML template literals
     },
 
     removePlaceholder(event) {
@@ -84,7 +146,7 @@ export default {
       }
     },
   }
-};
+});
 </script>
 
 <style>
@@ -103,10 +165,12 @@ export default {
 
 .button-container {
   margin-top: 10px;
+  white-space: nowrap;
 }
 
 .button-container button {
   margin-left: 5px;
+  display: inline-block;
 }
 
 .placeholder {
@@ -117,25 +181,37 @@ export default {
   background-color: #9e9e9e;
 }
 
-.stacked {
+.stacked,
+.buttonstacked {
   position: relative;
   display: inline-block;
 }
 
 .stacked sup,
-.stacked sub {
+.stacked sub,
+.buttonstacked sup,
+.buttonstacked sub {
   position: absolute;
   left: 0;
   display: block;
   text-align: center;
 }
 
+/* two different stacked ratios because the button looks weird if it's accurate */
+.buttonstacked sup {
+  top: -1.8em;
+}
+
+.buttonstacked sub {
+  bottom: -0.7em;
+}
+
 .stacked sup {
-  top: -0.3em;
+  top: -2em;
 }
 
 .stacked sub {
-  bottom: 0.7em;
+  bottom: -1.25em;
 }
 
 </style>
